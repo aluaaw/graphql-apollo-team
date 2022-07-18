14b06e23e994ed6ce21fde9427116a72a2832275
aluaaw <ylua.aw@gmail.com>
2022-07-18 17:51:24 +0900

    practice resolvers, typeDef, queries

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const database = require("./database");
 const { ApolloServer, gql } = require("apollo-server");
+const { equipments } = require("./database");
 const typeDefs = gql`
   type Query {
     teams: [Team]
+    teamsWithSupplies: [Team]
+    team(id: Int): Team
+    equipments: [Equipments]
+    supplies: [Supplies]
   }
   type Team {
     id: Int
@@ -12,11 +17,35 @@ const typeDefs = gql`
     mascot: String
     cleaning_duty: String
     project: String
+    supplies: [Supplies]
+  }
+  type Equipments {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supplies {
+    id: String
+    team: Int
   }
 `;
 const resolvers = {
   Query: {
     teams: () => database.teams,
+    teamsWithSupplies: () =>
+      database.teams.map((team) => {
+        team.supplies = database.supplies.filter((supply) => {
+          return supply.team === team.id;
+        });
+        return team;
+      }),
+    team: (parent, args, context, info) =>
+      database.teams.filter((team) => {
+        return team.id === args.id;
+      })[0],
+    equipments: () => database.equipments,
+    supplies: () => database.supplies,
   },
 };
 const server = new ApolloServer({ typeDefs, resolvers });
